75d86f66d37d8baa2c4a2a5f6dbe0fb9ff9daab0
kevinmartiniano <kevin.martiniano123@gmail.com>
2021-01-24 16:47:06 -0300

    Criando função para criação da struct com o life automático em 100.

--- lib/player.ex
+++ lib/player.ex
@@ -3,4 +3,13 @@ defmodule ExMon.Player do
 
   defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
 
+  def build(name, move_rnd, move_avg, move_heal) do
+    %ExMon.Player{
+      name: name,
+      move_rnd: move_rnd,
+      move_avg: move_avg,
+      move_heal: move_heal,
+      life: 100
+    }
+  end
 end
